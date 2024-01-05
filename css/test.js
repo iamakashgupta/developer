b37fef97deeec4810e5257ebd972880320eefcbd
iamakashgupta <akashg6980@gmail.com>
2024-01-05 19:30:15 +0530

    Initial commit

---
+++ css/test.js
@@ -0,0 +1,8 @@
+console.log("this is akash gupta, and I am engineer by profession.")
+const person = {
+    name: "akash gupta",
+    age: 20
+};
+
+console.log(person.name)
+console.log(person.age)
---
+++ js/jsscript.js
@@ -0,0 +1,52 @@
+function myCode1(){
+    console.log("hey mom, i am learning js")
+    console.log("hey, I  learning js on vs code")
+    console.log("i am akash gupta and i am learning js")
+    window.alert("so you are Akash gupta")
+
+    //alert and prompt
+    window.alert("hey akash")
+    a=window.prompt("type your name")
+}
+//myCode1();
+
+function myCode2(){
+    let firstName="bro";
+    let age=20;
+    let student= true;
+
+    console.log(firstName);
+    console.log(age);
+    console.log(student);
+
+    document.getElementById("p1").innerHTML="hello"+ firstName;
+    //arithmetic operation
+    //augmented assignment operator
+    let a =20;
+    console.log(a+=1);
+}
+//myCode2();
+
+
+function myCode3(){
+    console.log("this is the third function in this file")
+    
+}
+myCode3();
+function myCode4(){}
+function myCode5(){}
+function myCode6(){}
+function myCode7(){}
+function myCode8(){}
+function myCode9(){}
+function myCode10(){}
+function myCode11(){}
+function myCode12(){}
+function myCode13(){}
+function myCode14(){}
+function myCode16(){}
+function myCode16(){}
+function myCode17(){}
+function myCode18(){}
+function myCode19(){}
+function myCode20(){}
